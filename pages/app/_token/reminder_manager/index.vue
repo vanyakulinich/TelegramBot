afd9504220d79f5c60b43fc79c55bb5444aafef3
VanyaKulinich <kulinichvanya@gmail.com>
2019-02-08 01:27:29 +0200

    wip designing reminders main page

--- pages/app/_token/reminder_manager/index.vue
+++ pages/app/_token/reminder_manager/index.vue
@@ -1,15 +1,42 @@
 <template>
-  <v-list>
-    <template v-for="(item, itemKey) in reminders">
-      <v-subheader :key="itemKey">{{ item.text }}</v-subheader>
-    </template>
-  </v-list>
+  <div>
+    <ContentHeader text="My Reminders"/>
+    <div>filters by date and by expired</div>
+    <v-list>
+      <h3 class="header_list">
+        <div class="item_date_time">
+          <div>Date</div>
+          <div>Time</div>
+        </div>
+        <div class="item_text">Reminder Text</div>
+      </h3>
+      <v-divider class="header_divider"></v-divider>
+      <template v-for="(item, itemKey) in reminders">
+        <v-list-content :key="itemKey" class="item">
+          <div class="item_inner">
+            <div class="item_date_time">
+              <div>{{item.date}}</div>
+              <div>{{item.time}}</div>
+            </div>
+            <div class="item_text">{{ item.text }}</div>
+          </div>
+
+          <v-divider></v-divider>
+          <v-spacer></v-spacer>
+        </v-list-content>
+      </template>
+    </v-list>
+  </div>
 </template>
 
-<script>
+<script scopped>
 import { mapGetters } from "vuex";
+import ContentHeader from "../../../../components/headers/ContentHeader.vue";
 export default {
   name: "all_reminders",
+  components: {
+    ContentHeader
+  },
   computed: {
     ...mapGetters(["reminders"])
   }
@@ -17,6 +44,32 @@ export default {
 </script>
 
 <style>
+.header_list,
+.header_list *,
+.item,
+.item * {
+  display: flex;
+}
+.item {
+  flex-direction: column;
+}
+.item_inner {
+  color: rgba(255, 255, 255, 0.6);
+}
+.item_date_time {
+  justify-content: space-around;
+}
+.item_text {
+  padding-left: 10px;
+}
+v-divider {
+  height: 2px;
+  width: 100%;
+}
+.header_divider {
+  margin-bottom: 20px;
+  background-color: rgb(206, 208, 223);
+}
 </style>
 
 
